62c9069ace5c6e5368db0fcb5e21aea9cebf7d16
Jason J Wynne <jwynne@ackmanndickenson.com>
2014-03-07 07:36:31 -0600

    updated pages

--- wp-content/themes/thebe/404.php
+++ wp-content/themes/thebe/404.php
@@ -1,18 +1,7 @@
 <?php get_header(); ?>
 
-<h1>Page Not Found</h1>
-<p>
-  <strong>Did you type the URL?</strong><br />
-  You may have typed the address (URL) incorrectly. Check it to make sure you've got the exact right spelling, capitalization, etc.
-</p>
-<p>
-  <strong>Did you follow a link from somewhere else at this site?</strong><br />
-  If you reached this page from another part of this site.
-</p>
-<p>
-  <strong>Did you follow a link from another site?</strong><br />
-  Links from other sites can sometimes be outdated or misspelled.
-</p>
+<div class="four-oh-four">
+  <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/images/404_error.jpg" alt="Thebe & Co. 404 Error" />
+</div>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
